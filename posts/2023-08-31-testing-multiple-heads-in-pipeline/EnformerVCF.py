69b1828e983d3c75ced11cbd5f78666636d03b20
Sabrina Mi <s1mi@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2023-09-01 02:33:53 +0000

    add enformer vcf predict

---
+++ posts/2023-08-31-testing-multiple-heads-in-pipeline/EnformerVCF.py
@@ -0,0 +1,181 @@
+import tensorflow as tf
+# Make sure the GPU is enabled 
+import tensorflow_hub as hub # for interacting with saved models and tensorflow hub
+import joblib
+import gzip # for manipulating compressed files
+import kipoiseq # for manipulating fasta files
+from kipoiseq import Interval # same as above, really
+import pyfaidx # to index our reference genome file
+import pandas as pd # for manipulating dataframes
+import numpy as np # for numerical computations
+import matplotlib.pyplot as plt # for plotting
+import matplotlib as mpl # for plotting
+import seaborn as sns # for plotting
+import pickle # for saving large objects
+import os, sys # functions for interacting with the operating system
+
+import Bio
+from Bio.Seq import Seq
+def create_rev_complement(dna_string):
+    return(str(Seq(dna_string).reverse_complement()))
+
+import io
+import os
+import gzip
+
+
+targets_txt = 'https://raw.githubusercontent.com/calico/basenji/master/manuscripts/cross2020/targets_human.txt'
+df_targets = pd.read_csv(targets_txt, sep='\t')
+
+
+# @title `Enformer`, `EnformerScoreVariantsNormalized`, `EnformerScoreVariantsPCANormalized`,
+SEQUENCE_LENGTH = 393216
+
+class Enformer:
+
+  def __init__(self, tfhub_url):
+    self._model = hub.load(tfhub_url).model
+
+  def predict_on_batch(self, inputs):
+    predictions = self._model.predict_on_batch(inputs)
+    return {k: v.numpy() for k, v in predictions.items()}
+
+  @tf.function
+  def contribution_input_grad(self, input_sequence,
+                              target_mask, output_head='human'):
+    input_sequence = input_sequence[tf.newaxis]
+
+    target_mask_mass = tf.reduce_sum(target_mask)
+    with tf.GradientTape() as tape:
+      tape.watch(input_sequence)
+      prediction = tf.reduce_sum(
+          target_mask[tf.newaxis] *
+          self._model.predict_on_batch(input_sequence)[output_head]) / target_mask_mass
+
+    input_grad = tape.gradient(prediction, input_sequence) * input_sequence
+    input_grad = tf.squeeze(input_grad, axis=0)
+    return tf.reduce_sum(input_grad, axis=-1)
+
+
+class EnformerScoreVariantsRaw:
+
+  def __init__(self, tfhub_url, organism='human'):
+    self._model = Enformer(tfhub_url)
+    self._organism = organism
+
+  def predict_on_batch(self, inputs):
+    ref_prediction = self._model.predict_on_batch(inputs['ref'])[self._organism]
+    alt_prediction = self._model.predict_on_batch(inputs['alt'])[self._organism]
+
+    return alt_prediction.mean(axis=1) - ref_prediction.mean(axis=1)
+
+
+class EnformerScoreVariantsNormalized:
+
+  def __init__(self, tfhub_url, transform_pkl_path,
+               organism='human'):
+    assert organism == 'human', 'Transforms only compatible with organism=human'
+    self._model = EnformerScoreVariantsRaw(tfhub_url, organism)
+    with tf.io.gfile.GFile(transform_pkl_path, 'rb') as f:
+      transform_pipeline = joblib.load(f)
+    self._transform = transform_pipeline.steps[0][1]  # StandardScaler.
+
+  def predict_on_batch(self, inputs):
+    scores = self._model.predict_on_batch(inputs)
+    return self._transform.transform(scores)
+
+
+class EnformerScoreVariantsPCANormalized:
+
+  def __init__(self, tfhub_url, transform_pkl_path,
+               organism='human', num_top_features=500):
+    self._model = EnformerScoreVariantsRaw(tfhub_url, organism)
+    with tf.io.gfile.GFile(transform_pkl_path, 'rb') as f:
+      self._transform = joblib.load(f)
+    self._num_top_features = num_top_features
+
+  def predict_on_batch(self, inputs):
+    scores = self._model.predict_on_batch(inputs)
+    return self._transform.transform(scores)[:, :self._num_top_features]
+
+
+# TODO(avsec): Add feature description: Either PCX, or full names.
+
+
+# @title `variant_centered_sequences`
+
+class FastaStringExtractor:
+
+    def __init__(self, fasta_file):
+        self.fasta = pyfaidx.Fasta(fasta_file)
+        self._chromosome_sizes = {k: len(v) for k, v in self.fasta.items()}
+    #import pd.Interval as Interval
+    def extract(self, interval: Interval, **kwargs) -> str:
+        # Truncate interval if it extends beyond the chromosome lengths.
+        chromosome_length = self._chromosome_sizes[interval.chrom]
+        trimmed_interval = Interval(interval.chrom,
+                                    max(interval.start, 0),
+                                    min(interval.end, chromosome_length),
+                                    )
+        # pyfaidx wants a 1-based interval
+        sequence = str(self.fasta.get_seq(trimmed_interval.chrom,
+                                          trimmed_interval.start + 1,
+                                          trimmed_interval.stop).seq).upper()
+        # Fill truncated values with N's.
+        pad_upstream = 'N' * max(-interval.start, 0)
+        pad_downstream = 'N' * max(interval.end - chromosome_length, 0)
+        return pad_upstream + sequence + pad_downstream
+
+    def close(self):
+        return self.fasta.close()
+
+transform_path = 'gs://dm-enformer/models/enformer.finetuned.SAD.robustscaler-PCA500-robustscaler.transform.pkl'
+model_path = 'https://tfhub.dev/deepmind/enformer/1'
+model = Enformer(model_path) # here we load the model architecture.
+
+
+def one_hot_encode(sequence):
+  return kipoiseq.transforms.functional.one_hot_dna(sequence).astype(np.float32)
+
+
+
+# @title `plot_tracks`
+
+def plot_tracks(tracks, interval, height=1.5):
+  fig, axes = plt.subplots(len(tracks), 1, figsize=(20, height * len(tracks)), sharex=True)
+  for ax, (title, y) in zip(axes, tracks.items()):
+    ax.fill_between(np.linspace(interval.start, interval.end, num=len(y)), y)
+    ax.set_title(title)
+    sns.despine(top=True, right=True, bottom=True)
+  ax.set_xlabel(str(interval))
+  plt.tight_layout()
+
+def read_vcf(path):
+    with gzip.open(path, 'rt') as f:
+        lines = [l for l in f if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})
+
+def vcf_to_seq(target_interval, individual, vcf, fasta_extractor):
+  target_fa = fasta_extractor.extract(target_interval.resize(SEQUENCE_LENGTH))
+  window_coords = target_interval.resize(SEQUENCE_LENGTH)
+      # two haplotypes per individual
+  haplo_1 = list(target_fa[:])
+  haplo_2 = list(target_fa[:])
+
+  ref_mismatch_count = 0
+  for i,row in vcf.iterrows():
+    geno = row[individual].split("/")
+    if (row["POS"]-window_coords.start-1) >= len(haplo_2):
+      continue
+    if (row["POS"]-window_coords.start-1) < 0:
+      continue
+    if geno[0] == "1":
+      haplo_1[row["POS"]-window_coords.start-1] = row["ALT"]
+    if geno[1] == "1":
+      haplo_2[row["POS"]-window_coords.start-1] = row["ALT"]
+  return haplo_1, haplo_2
